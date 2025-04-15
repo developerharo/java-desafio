39b7b1548a515f5c45ee0de221f8dd37bdd48a11
developerharo <developerharo@gmail.com>
2025-04-14 23:28:46 -0600

    Actualizando Desafio.java, agregando el menú de opciones

--- src/Desafio.java
+++ src/Desafio.java
@@ -1,11 +1,14 @@
 
 import com.sun.security.jgss.GSSUtil;
 
+import java.util.Scanner;
+
 public class Desafio {
     public static void main(String[] args) {
         String nombre = "José Haro";
         String tipoDeCuenta = "Cheques";
         double saldo = 999999.00;
+        int opcion = 0;
 
         System.out.println("***************************");
         System.out.println("\n");
@@ -14,5 +17,19 @@ public class Desafio {
         System.out.println("Saldo: $ " + saldo);
         System.out.println("\n");
         System.out.println("***************************");
+
+        String menu = """
+                *** Escribe el número de la opción deseada ***
+                1 - Consultar saldo
+                2 - Retirar
+                3 - Depositar
+                9 - Salir
+                """;
+
+        Scanner teclado = new Scanner(System.in);
+        while (opcion != 9) {
+            System.out.println(menu);
+            opcion = teclado.nextInt();
+        }
     }
 }
