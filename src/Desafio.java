4c196e3ad0d8f43e4b578696af4ad39eb509818a
developerharo <developerharo@gmail.com>
2025-04-14 23:03:02 -0600

    Agregando README.md y Desafio.java donde se adicionaron los datos del cliente

---
+++ src/Desafio.java
@@ -0,0 +1,18 @@
+
+import com.sun.security.jgss.GSSUtil;
+
+public class Desafio {
+    public static void main(String[] args) {
+        String nombre = "José Haro";
+        String tipoDeCuenta = "Cheques";
+        double saldo = 999999.00;
+
+        System.out.println("***************************");
+        System.out.println("\n");
+        System.out.println("Cliente: " + nombre);
+        System.out.println("Tipo de cuenta: " + tipoDeCuenta);
+        System.out.println("Saldo: $ " + saldo);
+        System.out.println("\n");
+        System.out.println("***************************");
+    }
+}
